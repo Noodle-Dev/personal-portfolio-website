e5f6f9dfe0808995c111aecdb581222ebbaf8b71
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2024-12-01 00:31:07 -0600

    added New section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Hero = () =>{
+    return(
+    <section>
+        <div>
+            <h1>Hi, I'm Noodle AM</h1>
+            <p>I'm a game and web developer since 2020</p>
+            <a href="mailto:NoodleHairDev@protonmail.com"></a>
+        </div>
+    </section>);
+}
