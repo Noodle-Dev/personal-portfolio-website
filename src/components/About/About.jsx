27ca77344543bb56e2c9be5a87001bd1c4b9c060
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2024-12-01 23:08:35 -0600

    About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import styles from "./About.module.css"
+
 
 export const About = () =>{
     return (
-    <section>
-        <h2>About</h2>
-        <div><img src="assets/about/aboutImage.png" alt="Noodle sitting on a laptop"></img></div>
-    </section>);
+    <section className={styles.container}>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+            <img src="assets/about/aboutImage.png" alt="Noodle sitting on a laptop" className={styles.aboutImage} />
+            <ul className={styles.aboutImage}>
+                <li className={styles.aboutItem}>
+                    <img src="assets/about/cursorIcon.png" alt="Cursor Icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Game Developer</h3>
+                        <p>
+                            I'm a game developer with more thatn 5 years of experience
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src="assets/about/serverIcon.png" alt="server Icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Game Developer</h3>
+                        <p>
+                            I'm a game developer with more thatn 5 years of experience
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src="assets/about/cursorIcon.png" alt="Cursor Icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Game Developer</h3>
+                        <p>
+                            I'm a game developer with more thatn 5 years of experience
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    );
 }
