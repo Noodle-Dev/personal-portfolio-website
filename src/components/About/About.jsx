7a35618bd00008c2da7d32d6ee7e2a8f5ed04be0
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2024-12-02 22:16:32 -0600

    Added About Styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,11 +4,11 @@ import styles from "./About.module.css"
 
 export const About = () =>{
     return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}>
             <img src="assets/about/aboutImage.png" alt="Noodle sitting on a laptop" className={styles.aboutImage} />
-            <ul className={styles.aboutImage}>
+            <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
                     <img src="assets/about/cursorIcon.png" alt="Cursor Icon"/>
                     <div className={styles.aboutItemText}>
