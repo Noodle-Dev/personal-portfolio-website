18838473a43e7bd272ba18541f9f41fb04c9d5a7
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2024-12-03 20:47:03 -0600

    added Exp classes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import styles from "./Experience.module.css"
 
-import skills from "../../data/skills.json"
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+
 export const Experience = () =>{
     return(
-    <section id="experience">
-        <h2>Experience</h2>
-        <div>
-            <div>{
+    <section className={styles.container} id="experience">
+        <h2 className={styles.title} >Experience</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>{
                 skills.map((skills, id) => {
-                    return <div key={id}>
-                        <div> <img src={skills.imageSrc} alt={skills.title} /></div>
+                    return <div key={id} className={styles.skill}>
+                        <div className={styles.skillImageContainer}>
+                            <img src={skills.imageSrc} alt={skills.title}/>
+                        </div>
+                        <p>{skills.title}</p>
                     </div>
                 })
                 }</div>
-            <ul></ul>
+            <ul className={styles.history}>
+                {
+                    history.map((historyItem, id) => {
+                        return <li key={id} className={styles.historyItem}>
+                            <img src={historyItem.imageSrc} alt={`${historyItem.organisation} Logo`} />
+                            <div className={styles.historyItemDetails}>
+                                <h3> {`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                                <p> {`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                <ul> {historyItem.experiences.map((experience, id) => {
+                                    return <li key={id}> {experience} </li>
+                                })} </ul>
+                            </div>
+                        </li>
+                    })
+                }
+            </ul>
         </div>
     </section>);
     /* 1:10:20 */
